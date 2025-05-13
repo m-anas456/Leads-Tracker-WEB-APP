0179ab7804148a0db9a19228810aa022f49a611e
Muhammad Anas <167231172+m-anas456@users.noreply.github.com>
2025-05-13 12:56:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
-//FIREBASE DB SETUP 🔥
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js"
-import { getDatabase ,
-    ref,
-    push,
-    onValue,
-    remove} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-database.js"
-
- const firebaseConfig = {
-    databaseURL : "https://leads-tracker-9fac5-default-rtdb.asia-southeast1.firebasedatabase.app/"
- }
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app)
-const referenceinDB = ref(database, "leads")
-
-onValue(referenceinDB,function(snapshot){ //onvalue a func used to show all changes on DB
-    const snapshotExists = snapshot.exists() 
-    if(snapshotExists){
-        const snapshotValues = snapshot.val() //getting snapshot values only
-        let leadsArray = Object.values(snapshotValues) //converting object into array of values of object
-        console.log()
-        renderLeads(leadsArray)
-    }
-})
-
-//APP SETUP👏
-const inputbtn = document.querySelector("#input-btn")
+const btn = document.querySelector("#input-btn")
 const input = document.querySelector("#input-el")
 let listitems = document.querySelector("#list-items")
-
-inputbtn.addEventListener("click", function () {
-    push(referenceinDB,input.value)
-    input.value = "" 
-})
+let myLeads = []
+let leadsfromlocalstorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsfromlocalstorage)
+if (leadsfromlocalstorage) {
+    myLeads = leadsfromlocalstorage
+    renderLeads(myLeads) 
+}
 
 let delbtn = document.querySelector("#del-btn")
 delbtn.addEventListener("dblclick", () => {
-   remove(referenceinDB)
-   listitems.innerHTML = ""
-})    
+    myLeads = []
+    localStorage.clear()
+    renderLeads(myLeads) 
+})
+btn.addEventListener("click", function () {
+    myLeads.push(input.value)
+    input.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads) // Pass myLeads here
+})
 
 function renderLeads(leads) {
     let allinnerhtml = "" // Reset the variable to avoid duplication
